11b112d9553a6f38e1ea61fbcd6c341eb72b2c9d
cruelladevil <dev.timetravel@gmail.com>
2025-12-15 02:30:21 +0900

    feat: RecentPurchaseProducts 에러 처리

--- src/pages/HomePage/components/RecentPurchaseSection.tsx
+++ src/pages/HomePage/components/RecentPurchaseSection.tsx
@@ -1,6 +1,10 @@
 import { Flex, styled } from 'styled-system/jsx';
 import { Spacing, Text } from '@/ui-lib';
 import { ExchangedPriceText } from '@/components/ExchangedPriceText';
+import { QueryErrorResetBoundary } from '@tanstack/react-query';
+import { ErrorBoundary } from 'react-error-boundary';
+import ErrorSection from '@/components/ErrorSection';
+import { Suspense } from 'react';
 
 function RecentPurchaseSection() {
   return (
@@ -19,54 +23,73 @@ function RecentPurchaseSection() {
         }}
         direction={'column'}
       >
-        <Flex
-          css={{
-            gap: 4,
-          }}
-        >
-          <styled.img
-            src="/moon-cheese-images/cheese-1-1.jpg"
-            alt="item"
-            css={{
-              w: '60px',
-              h: '60px',
-              objectFit: 'cover',
-              rounded: 'xl',
-            }}
-          />
-          <Flex flexDir="column" gap={1}>
-            <Text variant="B2_Medium">월레스의 오리지널 웬슬리데일</Text>
-            <Text variant="H1_Bold">
-              <ExchangedPriceText price={12.99} />
-            </Text>
-          </Flex>
-        </Flex>
-
-        <Flex
-          css={{
-            gap: 4,
-          }}
-        >
-          <styled.img
-            src="/moon-cheese-images/cheese-2-1.jpg"
-            alt="item"
-            css={{
-              w: '60px',
-              h: '60px',
-              objectFit: 'cover',
-              rounded: 'xl',
-            }}
-          />
-          <Flex flexDir="column" gap={1}>
-            <Text variant="B2_Medium">그랜드 데이 아웃 체다</Text>
-            <Text variant="H1_Bold">
-              <ExchangedPriceText price={14.87} />
-            </Text>
-          </Flex>
-        </Flex>
+        <QueryErrorResetBoundary>
+          {({ reset }) => (
+            <ErrorBoundary
+              onReset={reset}
+              fallbackRender={({ resetErrorBoundary }) => <ErrorSection onRetry={resetErrorBoundary} />}
+            >
+              <Suspense fallback={null}>
+                <RecentPurchaseProducts />
+              </Suspense>
+            </ErrorBoundary>
+          )}
+        </QueryErrorResetBoundary>
       </Flex>
     </styled.section>
   );
 }
 
 export default RecentPurchaseSection;
+
+function RecentPurchaseProducts() {
+  return (
+    <>
+      <Flex
+        css={{
+          gap: 4,
+        }}
+      >
+        <styled.img
+          src="/moon-cheese-images/cheese-1-1.jpg"
+          alt="item"
+          css={{
+            w: '60px',
+            h: '60px',
+            objectFit: 'cover',
+            rounded: 'xl',
+          }}
+        />
+        <Flex flexDir="column" gap={1}>
+          <Text variant="B2_Medium">월레스의 오리지널 웬슬리데일</Text>
+          <Text variant="H1_Bold">
+            <ExchangedPriceText price={12.99} />
+          </Text>
+        </Flex>
+      </Flex>
+
+      <Flex
+        css={{
+          gap: 4,
+        }}
+      >
+        <styled.img
+          src="/moon-cheese-images/cheese-2-1.jpg"
+          alt="item"
+          css={{
+            w: '60px',
+            h: '60px',
+            objectFit: 'cover',
+            rounded: 'xl',
+          }}
+        />
+        <Flex flexDir="column" gap={1}>
+          <Text variant="B2_Medium">그랜드 데이 아웃 체다</Text>
+          <Text variant="H1_Bold">
+            <ExchangedPriceText price={14.87} />
+          </Text>
+        </Flex>
+      </Flex>
+    </>
+  );
+}
